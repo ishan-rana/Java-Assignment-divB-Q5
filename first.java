41ba81b0ba0c30d835665d1f8492f0597f60006b
ishan-rana <115205440+ishan-rana@users.noreply.github.com>
2022-11-23 14:41:06 +0530

    Add files via upload

---
+++ first.java
@@ -0,0 +1,14 @@
+public class hi {
+    public static void main (String args[])
+    {
+        System.out.print("III      III   HHHHHHHHHH\n"
+        + "III      III      HHH  \n"
+        + "III      III      HHH \n"
+        + "IIIIIIIIIIII      HHH \n"
+        + "IIIIIIIIIIII      HHH \n"
+        + "III      III      HHH \n"
+        + "III      III      HHH \n"
+        + "III      III   HHHHHHHHHH");
+    }
+    
+}
